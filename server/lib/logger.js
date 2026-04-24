10bf73dc6d04d7db5dfea754640128ab2a0e992c
luxidevil <112481539+luxidevil@users.noreply.github.com>
2026-04-25 01:42:40 +0530

    Add server/lib/logger.js

---
+++ server/lib/logger.js
@@ -0,0 +1,22 @@
+const pino = require("pino");
+
+const isProduction = process.env.NODE_ENV === "production";
+
+const logger = pino({
+  level: process.env.LOG_LEVEL || "info",
+  redact: [
+    "req.headers.authorization",
+    "req.headers.cookie",
+    "res.headers['set-cookie']",
+  ],
+  ...(isProduction
+    ? {}
+    : {
+        transport: {
+          target: "pino-pretty",
+          options: { colorize: true },
+        },
+      }),
+});
+
+module.exports = { logger };
