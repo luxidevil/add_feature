1aa3e81b8d1bda5b1a109ea1042f6f2e21a80517
luxidevil <112481539+luxidevil@users.noreply.github.com>
2026-04-25 01:42:45 +0530

    Add server/models/index.js

---
+++ server/models/index.js
@@ -0,0 +1,9 @@
+const { User } = require("./User");
+const { Log } = require("./Log");
+const { Setting } = require("./Setting");
+const { Voucher } = require("./Voucher");
+const { ImapCredential } = require("./ImapCredential");
+const { ProxyCredential } = require("./ProxyCredential");
+const { TopupTransaction } = require("./TopupTransaction");
+
+module.exports = { User, Log, Setting, Voucher, ImapCredential, ProxyCredential, TopupTransaction };
