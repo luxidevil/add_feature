cd8e6c3da60ef8c389ed9591251224f6c1f4b78b
luxidevil <112481539+luxidevil@users.noreply.github.com>
2026-04-25 01:42:29 +0530

    Add ecosystem.config.js

---
+++ ecosystem.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  apps: [{
+    name: "dealer-dxb",
+    script: "server/index.js",
+    env: {
+      NODE_ENV: "production",
+      PORT: 3000,
+    },
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: "500M",
+  }],
+};
